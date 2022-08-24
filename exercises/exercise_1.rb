6286b16c226f0e48eda94926decde05e7bdf8244
Yoyo Yang <yoyo.truly@gmail.com>
2022-08-23 20:16:14 -0400

    Add 3 stores to Store object

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,11 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+# Use Active Record's create class method multiple times to create 3 stores in the database
+
+Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+stores = Store.all
+puts stores.count
